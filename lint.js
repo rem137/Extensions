a15f3db8346c2e7790633281b7b43fd7182c4ce7
Bibliofile <Bibliofilegit@gmail.com>
2017-10-13 22:58:52 -0600

    Add linter, update to include up to date extensions

---
+++ lint.js
@@ -0,0 +1,51 @@
+const { readFileSync, readdirSync, writeFileSync } = require('fs')
+const assert = require('assert')
+
+let keys = [
+    'user',
+    'id',
+    'package',
+    'title',
+    'description',
+    'env'
+]
+
+function isValidEnv(env) {
+    return env.replace(/\s|\+|all|browser|cli|cloud|mac|/g, '').length === 0
+}
+
+function validateJSON(json) {
+    assert.ok(Array.isArray(json), 'JSON must be an array')
+
+    let seen = []
+
+    json.forEach(ex => {
+        let id = ex.id || JSON.stringify(ex)
+
+        assert.deepEqual(Object.keys(ex), keys, `Missing key or extra key for ${id}`)
+        assert.ok(isValidEnv(ex.env), `Invalid env for ${id}`)
+        assert.ok(!ex.package.includes('github.com'), `package should not link directly to github, ${id}`)
+
+        if (seen.includes(id)) {
+            assert.fail(`Duplicate id ${id}`)
+        }
+        seen.push(id)
+    })
+}
+
+function sortExtensions(json) {
+    return json.sort((a, b) => a.title.localeCompare(b.title, 'standard', { sensitivity: 'base' }))
+}
+
+readdirSync('.')
+    .filter(file => file.endsWith('.json'))
+    .forEach(file => {
+        try {
+            let content = JSON.parse(readFileSync(file))
+            validateJSON(content)
+            writeFileSync(file, JSON.stringify(sortExtensions(content), null, 4) + '\n')
+            console.log(`OK [${file}]: Lint passed, formatted file.`)
+        } catch (error) {
+            console.log(`ERROR [${file}]: `, error.message)
+        }
+    })
